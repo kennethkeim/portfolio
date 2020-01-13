00c39f138f0f2bc3b4bcbe7f1c1c76bb76d5131c
Kenneth Keim <kkeim77@gmail.com>
2020-01-12 20:22:17 -0500

    moved project data to json

---
+++ src/app/data/projects.ts
@@ -0,0 +1,43 @@
+
+const projects = [
+   {
+      name: 'VIP Puppies',
+      shortDesc: 'web and native apps',
+      link: 'https://vippuppies.com',
+      image: '~/../assets/images/projects/dog.jpg',
+      problem: '',
+      solution: ''
+   }, {
+      name: 'iCare Greetings',
+      shortDesc: 'order management',
+      link: 'https://icaregreetings.com',
+      image: '',
+      problem: '',
+      solution: ''
+   }, {
+      name: 'Keim Industrial Surplus',
+      shortDesc: 'website',
+      link: 'https://keimindustrialsurplus.com',
+      image: '~/../assets/images/projects/keim.png',
+      problem: '',
+      solution: ''
+   }, {
+      name: 'Swiss Maid Pastries',
+      shortDesc: 'order management',
+      link: 'https://swissmaidpastries.com',
+      image: '',
+      problem: '',
+      solution: ''
+   }, {
+      name: 'Abner Henry Fine Furniture',
+      shortDesc: 'inventory management',
+      link: 'https://abnerhenry.com/slabs',
+      image: '~/../assets/images/projects/ah.jpg',
+      problem: '',
+      solution: ''
+   }
+];
+
+
+
+export { projects };
--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { projects as allProjects } from '../../data/projects';
+
+
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.html',
   styleUrls: ['./projects.scss']
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+   public projects = allProjects;
+
+}
