45b2b33fc2c736bfffe8f168d1f61bef1bc1304a
Kenneth Keim <kkeim77@gmail.com>
2020-03-28 22:48:18 -0400

    first draft of project descriptions

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -5,29 +5,29 @@ const projects = [
       shortDesc: 'web and native apps',
       link: 'https://vippuppies.com',
       image: '~/../assets/images/projects/vipp.jpeg',
-      problem: `VIP Puppies needed to migrate from their existing WordPress site. They also needed IOS and Android apps.`,
-      solution: 'Working with a software startup, I was able to provide web and native apps in a cost effective manner using a shared codebase.'
+      problem: `VIP Puppies needed to migrate from their existing WordPress e-commerce site. They also needed IOS and Android apps.`,
+      solution: 'Working with Electron Innovations, I was able to provide web and native apps in a cost effective manner using a shared codebase.'
    }, {
       name: 'Keim Industrial Surplus',
       shortDesc: 'website',
       link: 'https://keimindustrialsurplus.com',
       image: '~/../assets/images/projects/keim.png',
-      problem: '',
-      solution: ''
+      problem: 'Keim Industrial Surplus asked me to build a small website with some business and contact information.',
+      solution: 'I created a simple one page site, writing everything from scratch in order to deploy quickly with good SEO.',
    }, {
       name: 'iCare Greetings',
-      shortDesc: 'order management',
+      shortDesc: 'command line interface',
       link: 'https://icaregreetings.com',
       image: '~/../assets/images/projects/icare.jpg',
-      problem: '',
-      solution: ''
+      problem: 'iCare Greetings needed a better way to buy postage and combine pdf templates for printing their greeting cards.',
+      solution: 'Working with Electron Innovations, I built a cli program to get orders from BigCommerce, purchase and download shipping labels, and combine the card templates and shipping labels into a print ready pdf.'
    }, {
       name: 'Abner Henry Fine Furniture',
-      shortDesc: 'inventory display',
+      shortDesc: 'backend for inventory page',
       link: 'https://abnerhenry.com/slabs',
       image: '~/../assets/images/projects/ah.jpg',
-      problem: '',
-      solution: ''
+      problem: 'Abner Henry needed a way to dynamically update their inventory from a third party supplier, who kept the inventory list in an excel file.',
+      solution: 'Using the existing systems of the supplier along with modern web technology, I was able to keep the inventory data live in a way that was satisfactory for both Abner Henry and the supplier.'
    }
 ];
 
