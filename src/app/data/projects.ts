3962fcde7e3820982b58f70057fda5aaae79dbe7
Kenneth Keim <kkeim77@gmail.com>
2020-07-12 14:24:43 -0400

    implement grammarly suggestions

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,7 +3,7 @@ const featProject = {
    name: 'VIP Puppies',
    link: '', // https://vippuppies.com
    image: '~/../assets/images/vipp-sm.png',
-   description: "VIP Puppies is a full service puppy marketplace with integrated payments and financing, in-app messaging, Android and IOS apps, and a blog. I've served as the lead developer on this project for over a year, working for Electron Innovations. I'll add a link here once this project goes live!"
+   description: "VIP Puppies is a full-service puppy marketplace with integrated payments and financing, in-app messaging, Android and IOS apps, and a blog. I've served as the lead developer on this project for over a year, working for Electron Innovations. I'll add a link here once this project goes live!"
 }
 
 const projects = [
@@ -14,11 +14,11 @@ const projects = [
    }, {
       name: 'iCare Greetings',
       link: 'https://icaregreetings.com',
-      description: 'Working with Electron Innovations, I developed a backend system to automate fetching orders, buying postage, and combining shipping labels and greeting card templates into a print ready pdf.'
+      description: 'Working with Electron Innovations, I developed a backend system to automate fetching orders, buying postage, and combining shipping labels and greeting card templates into a print-ready pdf.'
    }, {
       name: 'Abner Henry Fine Furniture',
       link: 'https://abnerhenry.com/slabs',
-      description: 'Abner Henry Fine Furniture needed a way to dynamically update their inventory from a third party supplier, who kept the inventory list in an excel file. I deployed a web server that parses iventory items from the excel file and serves it to their website.'
+      description: 'Abner Henry Fine Furniture needed a way to dynamically update their inventory from a third party supplier, who kept the inventory list in an excel file. I deployed a web server that parses inventory items from the excel file and serves it to their website.'
    }
 ];
 
