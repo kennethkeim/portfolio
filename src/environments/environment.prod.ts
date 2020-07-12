7bf3784e8715edddcc496ad618b4607d32759470
Kenneth Keim <kkeim77@gmail.com>
2020-07-11 20:02:55 -0400

    update api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-   api: 'https://us-central1-kennethkeim-api.cloudfunctions.net/portfolio-contact-form'
+   production: true,
+   api: "https://us-central1-kennethkeim-api.cloudfunctions.net/contactMe"
 };
