8d4109a321c169ec6f2ae3206dec0d90d79623c7
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2022-08-27 14:40:54 -0400

    chore: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 90,
+  tabWidth: 3,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  // fingers crossed
+  // https://github.com/prettier/prettier/issues/5377#issuecomment-678644985
+  // bracketSameLine: true,
+  arrowParens: "always",
+  rangeStart: 0,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "auto",
+};
