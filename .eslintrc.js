45b19340c547de661abca3fb131f3702d7bab5e7
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2022-08-27 14:36:21 -0400

    chore: add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,77 @@ module.exports = {
             style: "kebab-case",
           },
         ],
+        curly: ["warn", "multi-line", "consistent"],
+        eqeqeq: "error",
+        // git on windows checks out CRLF and commits LF
+        "linebreak-style": [
+          process.platform === "win32" ? "off" : "error",
+          "unix",
+        ],
+        // "multiline-comment-style": ["warn", "starred-block"],
+        "no-lonely-if": ["warn"],
+        "no-use-before-define": "off", // use the rule from typescript-eslint
+        "no-await-in-loop": "warn",
+        "@typescript-eslint/dot-notation": "off",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/member-delimiter-style": [
+          "warn",
+          {
+            multiline: {
+              delimiter: "none",
+            },
+          },
+        ],
+        "@typescript-eslint/member-ordering": ["warn"],
+        "@typescript-eslint/no-confusing-non-null-assertion": ["warn"],
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-use-before-define": [
+          "error",
+          {
+            enums: true,
+            typedefs: true,
+            ignoreTypeReferences: false,
+          },
+        ],
+        "@typescript-eslint/prefer-for-of": ["warn"],
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/promise-function-async": "off", // flags angular lazy-loaded imports
+        "@typescript-eslint/naming-convention": [
+          "warn",
+          {
+            selector: "default",
+            format: ["camelCase"],
+          },
+          {
+            selector: "typeLike",
+            format: ["PascalCase"],
+          },
+          {
+            // allow leading _ for private class properties
+            selector: "classProperty",
+            modifiers: ["private"],
+            format: null,
+          },
+          {
+            // ignore formatting if requires quotes
+            selector: [
+              "classProperty",
+              "objectLiteralProperty",
+              "typeProperty",
+              "classMethod",
+              "objectLiteralMethod",
+              "typeMethod",
+              "accessor",
+              "enumMember",
+            ],
+            format: null,
+            modifiers: ["requiresQuotes"],
+          },
+        ],
       },
     },
     {
