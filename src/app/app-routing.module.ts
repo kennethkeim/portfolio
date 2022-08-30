f734b2a63b91a65b46c3387f7cc35722de9d6a8a
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2022-08-30 06:52:18 -0400

    feat: add not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
+import { NotFoundComponent } from "./not-found/not-found.component"
 
 const routes: Routes = [
    {
       path: "",
       loadChildren: () => import("./home/home.module").then((m) => m.HomeModule)
+   },
+   {
+      path: "**",
+      component: NotFoundComponent
    }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app/app.component"
+import { NotFoundComponent } from "./not-found/not-found.component"
+import { AppCommonModule } from "./shared/app-common/app-common.module"
 
 @NgModule({
-   declarations: [AppComponent],
-   imports: [BrowserModule, AppRoutingModule],
+   declarations: [AppComponent, NotFoundComponent],
+   imports: [BrowserModule, AppRoutingModule, AppCommonModule, CommonModule],
    providers: [],
    bootstrap: [AppComponent]
 })
