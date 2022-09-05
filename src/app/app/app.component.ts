f86a910eb3b42349e54c7eacdf23baba523e60c4
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2022-09-05 12:03:04 -0400

    feat: scroll to top on nav end

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { NavigationEnd, Router } from "@angular/router"
+import { filter } from "rxjs"
 
 @Component({
    selector: "app-root",
    template: `<router-outlet></router-outlet>`,
    styles: []
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+   constructor(private router: Router) {}
+
+   ngOnInit(): void {
+      this.router.events
+         .pipe(filter((event) => event instanceof NavigationEnd))
+         .subscribe(() => {
+            // dom content needs a few ms to render correct page height
+            setTimeout(() => window.scrollTo({ left: 0, top: 0 }), 5)
+         })
+   }
+}
