d9708d0afe3678baaec22aa51fa45d107167e095
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2026-01-25 00:33:05 -0500

    Update bio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core"
 import { projects } from "../projects/projects"
 import { hobbies } from "./hobbies"
 import { mainSkills, otherSkills } from "./skills"
+import { TimeService } from "../shared/app-common/time.service"
+import { map } from "rxjs"
 
 @Component({
    selector: "app-home",
@@ -14,7 +16,15 @@ export class HomeComponent implements OnInit {
    hobbies = hobbies
    projects = projects.filter((p) => p.showOnHomePage)
 
-   constructor() {}
+   yoeRounded$
+
+   constructor(public timeSvc: TimeService) {
+      this.yoeRounded$ = timeSvc.yoe$.pipe(
+         map((yoe) => {
+            return Math.floor(Number(yoe))
+         })
+      )
+   }
 
    ngOnInit(): void {}
 }
