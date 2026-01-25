55de8934c9787c671659c2b4594f88164c35a6d1
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2026-01-25 01:02:35 -0500

    Update skills

--- src/app/home/skills.ts
+++ src/app/home/skills.ts
@@ -1,36 +1,11 @@
 export const mainSkills: string[] = [
-   // languages (kind of)
-   "TypeScript",
-   "Node.js",
-
-   // frameworks and libs
-   "Angular",
+   "Postgres",
    "Next.js",
-   "Tailwind CSS",
-
-   // cloud, db
-   "AWS",
-   "DynamoDB"
+   "React Native (Expo)",
+   "Vercel",
+   "AWS"
 ]
 
-export const otherSkills: string[] = [
-   // languages
-   "Python",
-   "C",
-
-   // frameworks and libs
-   "NativeScript",
-   "Express",
-   "Socket.io",
-   "Passport.js",
-   "Stripe",
-   "Angular Material",
-
-   // cloud, db
-   "MongoDB",
-   "PostgreSQL",
-   "SQLite",
-   "Firestore"
-]
+export const otherSkills: string[] = ["DynamoDB", "Express", "Angular", "Node.js"]
 
 export const allSkills: string[] = [...mainSkills, ...otherSkills]
